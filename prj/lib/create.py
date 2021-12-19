803e306c57a2e27b0d1760f3bb1c26feebd66d78
leeoos <leeoos@pop-os.localdomain>
2021-12-19 16:57:33 +0100

    multiple rection support

--- prj/lib/create.py
+++ prj/lib/create.py
@@ -117,8 +117,8 @@ def make_lmp(**kwargs):
     species = model.getNumSpecies()
 
     r = ReactionValues(model)
-    print(r.reactants)
-    print(r.products)
+    reactants = [item for sublist in r.reactants for item in sublist]
+    print(reactants)
     products = [item for sublist in r.products for item in sublist]
     print(products)
     r_dictionary = r.dictionary
@@ -167,7 +167,7 @@ def make_lmp(**kwargs):
 
         "# define simulation box",
         "region      box block 0 30 0 30 0 30",
-        "create_box  "+str(species)+" box  bond/types 1 extra/bond/per/atom 100\n",
+        "create_box  "+str(species)+" box  bond/types " + str(len(r.reactants)) + " extra/bond/per/atom 100\n",
 
         "# create simulation walls",
         "fix xwalls all wall/reflect xlo EDGE xhi EDGE",
@@ -180,9 +180,12 @@ def make_lmp(**kwargs):
         f.write("\n#       --- AGENTS PROPRETIES AND FORCE FIELDS ---\n")
         f.write("# creation of atoms of types in randoms spots inside the box\n")
         
-        for i in list(r_dictionary.values()):
-            f.write("create_atoms" + "    " + str(i) + " random ${atoms} " + str(r_seed) + " box")
-            r_seed = r_seed + i
+        for k in list(r_dictionary.keys()):
+            if k in products:
+                f.write("create_atoms" + "    " + str(r_dictionary[k]) + " random 0 " + str(r_seed) + " box  #product")
+            else:
+                f.write("create_atoms" + "    " + str(r_dictionary[k]) + " random ${atoms} " + str(r_seed) + " box")
+            r_seed = r_seed + 1
             f.write("\n")
 
         f.write("\n# atoms mass\n")
@@ -191,10 +194,18 @@ def make_lmp(**kwargs):
             f.write("\n")
 
         f.write("\n# assing atoms to cerian groups\n")
-        for i in list(r_dictionary.values()) :
-            f.write("group g" + str(i) + " type "+str(i))
-            f.write("\ngroup agents  union agents g" +str(i))
+        for k in list(r_dictionary.keys()) :
+            if k in reactants:
+                f.write("group g" + str(r_dictionary[k]) + " type "+str(r_dictionary[k]))
             f.write("\n")
+        j=1
+        for i in r.reactants:
+            group_name = "group agents"+str(j)+" union "
+            j += 1
+            for k in i: group_name = group_name + "g"+str(r_dictionary[k]) + " "
+            f.write(group_name)
+            f.write("\n")
+
 
         ag_prop = [
         "\n# force fields style and coefficient",
@@ -208,7 +219,7 @@ def make_lmp(**kwargs):
         f.writelines(["%s\n" % item  for item in ag_prop])
         
         # SIMULATION 
-        sim = [
+        sim1 = [
         " \n#      --- SIMULATION ---\n",
 
         "# set time steps ",
@@ -226,12 +237,12 @@ def make_lmp(**kwargs):
         "# this fix will attempt to create new bond btw atoms of ",
         "# type 1 and 2 every Nevery timestep"]
 
-        f.writelines(["%s\n" % item  for item in sim])
+        f.writelines(["%s\n" % item  for item in sim1])
 
         j = 1
         for i in range(0, r.num_of_reactions):
             for k in r.arrange_reaction(i) :
-                f.write("fix bond"+str(j)+" all bond/create 10 "+ str(k[0]) +" "+ str(k[1])+ " 1.0 1 prob 0.5 " + str(r_seed))
+                f.write("fix bond"+str(j)+" all bond/create 10 "+ str(k[0]) +" "+ str(k[1])+ " 1.0 "+ str(i+1) +" prob 0.5 " + str(r_seed))
                 j += 1
                 r_seed = r_seed + j
                 f.write("\n")
@@ -247,30 +258,48 @@ def make_lmp(**kwargs):
         "fix 2 all langevin 300.0 300.0 10.0 904297\n",
         
         "# compute if atoms has a bonds",
-        "# and total number of bonds btw all atoms",
-        "compute 1 agents property/atom nbonds",
-        "compute 2 agents reduce sum c_1 ",           
-        "thermo_style custom step temp pe c_2",
-        "run 0",
-        "# this lines are necessary to insure that the “hasbond” and 'newatoms' ",
-        "# variables are current when the group command invokes it.\n",
-        
-        "# hasbond : boolean = true if atom I has a bond with atom J",
-        "variable hasbond atom 'c_1 > 0.0'\n",
-        
-        "# bondcounter : int = N total number of bonds in the sim",
-        "variable bondcounter equal ceil(c_2) \n",
-        
-        "# print themo info every timestep ",
-        "thermo_style custom step temp pe v_bondcounter\n",
-        
-        "# dumps atoms information ",
-        "dump 1 all custom 10 dump.out id x y z type \n"]
+        "# and total number of bonds btw all atoms"]
 
         f.writelines(["%s\n" % item  for item in sim2])
 
+        thermo_style1 = "thermo_style custom step temp pe "
+        thermo_style2 = "thermo_style custom step temp pe "
+        counter = "variable counter"
+
+        f.write("compute hb0 all property/atom nbonds \n\n")
+
+        for i in range(1, len(r.reactants)+1):
+            f.write("compute hb"+str(i)+" agents"+str(i)+" property/atom nbonds\n")
+            f.write("compute cb"+str(i)+" agents"+str(i)+" reduce sum c_hb"+str(i)+"\n")
+
+            thermo_style1 = thermo_style1 + "c_cb"+str(i) +" "
+            counter = counter + str(i) + " equal ceil(c_cb" + str(i) + ")\nvariable counter"
+            thermo_style2 = thermo_style2 + "v_counter" + str(i) + " "
+
+            f.write("\n")
+
+        sim3 = [
+        "\n# this lines are necessary to insure that the “hasbond” and 'newatoms' ",
+        "# variables are current when the group command invokes it.",          
+        thermo_style1,
+        "run 0",
+
+        "\n# hasbond : boolean = true if atom I has a bond with atom J",
+        "variable hasbond atom 'c_hb0 > 0.0'",
+
+        "\n# counter : int = N total number of bonds in the sim",
+        counter[:counter.rfind('\n')],
+        
+        "\n# print themo info every timestep",
+        thermo_style2,
+        
+        "\n# dumps atoms information",
+        "dump 1 all custom 10 dump.out id x y z type \n"]
+
+        f.writelines(["%s\n" % item  for item in sim3])
+
         # LOOP 
-        loop = [
+        loop1 = [
         "\n#       --- LOOP---\n",
         
         "label loop",
@@ -278,12 +307,25 @@ def make_lmp(**kwargs):
         
         "# create new atoms only if new bonds have been made", 
         "# the num of new atoms is linked to the number of new bonds as follow:",
-        "variable newatoms equal floor(${bondcounter}/2)",
-        "if '${bondcounter} > 0' then &",
+        "# fix ID group-ID deposit N type M seed keyword values"]
+
+        f.writelines(["%s\n" % item  for item in loop1])
+
+        i = 1
+        for p in products:
+            f.write("variable newatoms"+str(i)+" equal floor(${counter"+str(i)+"}/2)\n")
+            f.write("if '${counter"+str(i)+"} > 0' then &\n")
+            f.write("'fix depositatoms all deposit ${newatoms"+str(i)+"} "+str(r_dictionary[p])+" 1 5748 region box near 2.0' \n")
+            f.write("\n")
+            i += 1
+
+        #"variable newatoms equal floor(${bondcounter}/2)",
+        #"if '${bondcounter} > 0' then &",
         "'fix depositatoms all deposit ${newatoms} 6 1 5748 region box near 2.0' ",
-        "# fix ID group-ID deposit N type M seed keyword values\n",
+        "\n",
         
-        "# assing all atoms that have a bond to the garbage group",
+        loop2 = [
+        "\n# assing all atoms that have a bond to the garbage group",
         "group garbage dynamic all every 1 var hasbond\n",
         
         "# append new values on dump file",
@@ -309,7 +351,7 @@ def make_lmp(**kwargs):
         "print 'Duration: ${duration}'",
         "print 'ALL DONE' \n"]
 
-        f.writelines(["%s\n" % item  for item in loop])
+        f.writelines(["%s\n" % item  for item in loop2])
 
         f.flush()
         os.fsync(f)
