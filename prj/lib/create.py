d5b65a24159a5436cbc945d77155c245930109e7
leeoos <leoanrdo.colosi@gmail.com>
2022-01-20 00:42:32 +0100

    almost done

--- prj/lib/create.py
+++ prj/lib/create.py
@@ -29,13 +29,8 @@ class SpeciesClass:
             specie = model.getSpecies(i)
             value = specie.getInitialConcentration()
             if (math.isnan(value)) : scale = False; break
-            else : amounts.append(value)
-        if scale:
-            # compute a reduced number of moles
-            my_n_moli = (reduce(lambda x,y: x+y, amounts))/self.num_of_species * 10
-            for i in range(len(amounts)):
-                amounts[i] = math.ceil(amounts[i] * my_n_moli)
-        else:
+            else : amounts.append(math.ceil(value) * 10)
+        if not(scale):
             amounts = []
             for i in range(self.num_of_species):
                 specie = model.getSpecies(i)
@@ -183,7 +178,7 @@ def make_lmp(**kwargs):
     if(lmp_file_path == None) :  lmp_file_path = 'in.lmp'
 
     if (sbml_model_file == None) :
-        test = 'test.xml'
+        test = 'Alharbi2020.xml'
         if(os.path.isfile('/home/leeoos/Projects/Tesi/AB-Sim-Of-Bio-Systems/models/'+test)) : 
             sbml_model_file = '/home/leeoos/Projects/Tesi/AB-Sim-Of-Bio-Systems/models/'+test #Alharbi2020
         else: 
@@ -215,8 +210,8 @@ def make_lmp(**kwargs):
             print(prod,"("+str(S.dictionary[prod][0])+")",  end=' ')
         print()
         print("\n")
-    
 
+ 
     with open(lmp_file_path, 'w') as f:
         f.write('# Agent Based Simulation Of Biological Systems\n\n')
     
@@ -302,16 +297,17 @@ def make_lmp(**kwargs):
         mortals = "" 
         for i in range (0, R.num_of_reactions):
             new_group = "group agents" + str(i+1) + " type " ; types = ""
-            toDie = False 
-
+            check = len(R.groups_of_reactants[i]) 
             if (R.groups_of_reactants[i] == []) : pass
             else :
-                if (R.groups_of_products[i] == []) : toDie = True 
+                if (R.groups_of_products[i] == []) : check = check - 1 
                 for k in R.groups_of_reactants[i]:
                     types = types + str(S.dictionary[k][0]) + " "
-                    if toDie : mortals = mortals + " " + str(S.dictionary[k][0]) 
-                f.write(new_group + types)
-                f.write("\n")
+                    if (check == 0) : 
+                        mortals = mortals + " " + str(S.dictionary[k][0]) 
+                if (check != 0) : 
+                    f.write(new_group + types)
+                    f.write("\n")
 
         if (mortals != "") :
             f.write("group mortals type"+ mortals +" \n") 
@@ -388,16 +384,18 @@ def make_lmp(**kwargs):
         f.write("compute hb0 all property/atom nbonds \n\n")
 
         for i in range(1, R.num_of_reactions+1):
-            if (R.groups_of_reactants[i-1] == []) : pass
+            if (len(R.groups_of_reactants[i-1]) <= 1) : pass
             else:
                 f.write("compute hb"+str(i)+" agents"+str(i)+" property/atom nbonds\n")
                 f.write("compute cb"+str(i)+" agents"+str(i)+" reduce sum c_hb"+str(i)+"\n")
 
                 thermo_style1 = thermo_style1 + "c_cb"+str(i) +" "
-                counter = counter + str(i) + " equal ceil(c_cb" + str(i) + ")\nvariable counter"
+                counter = counter + str(i) + " equal ceil(c_cb" + str(i) + ")\nvariable counter" #da malati
                 thermo_style2 = thermo_style2 + "v_counter" + str(i) + " "
 
                 f.write("\n")
+
+        if (counter == "variable counter" ) : counter = "# no bonds detected "
 
         sim3 = [
         "\n# this lines are necessary to insure that the “hasbond” and 'newatoms' ",
@@ -437,22 +435,26 @@ def make_lmp(**kwargs):
 
         for i in range(len(R.groups_of_products)):
             deposit = []
-            if (R.groups_of_products[i] == []) : pass
+            if (len(R.groups_of_reactants[i]) <= 1 
+                    and R.groups_of_products[i] == []) :  pass
             else :
+                if (R.groups_of_products[i] == []) : deposit.append(0)
                 for p in R.groups_of_products[i] :
-                    if (R.groups_of_reactants[i] == []):
-                        f.write("fix deposit"+str(i+1)+" all deposit 1 "+str(S.dictionary[p][0])
+                    if (R.groups_of_reactants[i] == []) :
+                        f.write("fix deposit"+str(i+1)+" all deposit 1 "+ str(S.dictionary[p][0])
                                     +" 1 5748 region box near 2.0\n")
+                        deposit.append(0)
                     else:
                         atom = S.dictionary[p][0]
-                        deposit.append("'fix deposit"+str(atom)+" all deposit ${newatoms"+str(i+1)+"} "+str(atom)
+                        deposit.append("'fix deposit"+ str(atom) +" all deposit ${newatoms"+ str(i+1) +"} "+ str(atom)
                                        +" 1 5748 region box near 2.0' &")
-                f.write("variable newatoms"+str(i+1)+" equal floor(${counter"+str(i+1)+"}/2)\n")
-                f.write("if '${counter"+str(i+1)+"} > 0' then &\n")
-                deposit[len(deposit)-1] = deposit[len(deposit)-1][:-1]
-                f.writelines(["%s\n" % item  for item in deposit])
-                #f.write("'fix deposit"+str(i+1)+" all deposit ${newatoms"+str(i+1)+"} "+str(S.dictionary[p][0])
-                #            +" 1 5748 region box near 2.0' \n")
+
+                if (deposit[0] == 0) : pass
+                else:
+                    f.write("variable newatoms"+str(i+1)+" equal floor(${counter"+str(i+1)+"}/2)\n")
+                    f.write("if '${counter"+str(i+1)+"} > 0' then &\n")
+                    deposit[len(deposit)-1] = deposit[len(deposit)-1][:-1]
+                    f.writelines(["%s\n" % item  for item in deposit])
 
         if(S.total_amount == 0):
             to_dump = ("# assing all atoms of the right kind to the dump group\n"
@@ -463,7 +465,7 @@ def make_lmp(**kwargs):
         if (mortals != "") :
             mortals = "# delate some atoms in mortals every N timestamps \n"
             mortals = mortals + "# fix ID group-ID evaporate N M region-ID seed \n"
-            mortals = mortals + "fix death mortals evaporate 100 1 box "+ str(r_seed)
+            mortals = mortals + "fix death mortals evaporate 100 1 box "+ str(r_seed) +"\n"
                             
         
         loop2 = [
@@ -483,7 +485,7 @@ def make_lmp(**kwargs):
 
         mortals,
         
-        "\n# jump to loop lable until step > 0 ",
+        "# jump to loop lable until step > 0 ",
         "next step",
         "jump SELF loop\n",
         
